8280bb823d06bc97d460840734391f80812ec0f5
Tom Scogland <scogland1@llnl.gov>
2023-11-10 11:11:11 -0800

    format and fix test version of rust

--- var/spack/repos/builtin/packages/rust/package.py
+++ var/spack/repos/builtin/packages/rust/package.py
@@ -91,19 +91,18 @@ class Rust(Package):
         ar = which("ar", required=True)
         env.set("AR", ar.path)
 
-        output = Executable("openssl")("version", "-d", output=str, error=str)
-        ossl = self.spec["openssl"]
-        m = re.match('OPENSSLDIR: "([^"]+)"', output)
-        if m:
-            cert_dir = m.group(1)
-        elif ossl.external and ossl.prefix == "/usr":
-            cert_dir = "/etc"
-        else:
-            cert_dir = join_path(ossl.prefix, "etc")
+        # output = Executable("openssl")("version", "-d", output=str, error=str)
+        # ossl = self.spec["openssl"]
+        # m = re.match('OPENSSLDIR: "([^"]+)"', output)
+        # if m:
+        #     cert_dir = m.group(1)
+        # elif ossl.external and ossl.prefix == "/usr":
+        #     cert_dir = "/etc"
+        # else:
+        #     cert_dir = join_path(ossl.prefix, "etc")
         # Manually inject the path of openssl's certs for build.
-
-    # certs = join_path(cert_dir, "openssl/cert.pem")
-    # env.set("CARGO_HTTP_CAINFO", certs)
+        # certs = join_path(cert_dir, "openssl/cert.pem")
+        # env.set("CARGO_HTTP_CAINFO", certs)
 
     def configure(self, spec, prefix):
         opts = []
